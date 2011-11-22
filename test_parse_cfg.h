09296bd556347cf97cef49d2d18a1337810fbbbb
Alex <theworldcreator@gmail.com>
2011-11-23 01:27:21 +0400

    header constants

--- test_parse_cfg.h
+++ test_parse_cfg.h
@@ -1,1 +1,8 @@
+#ifndef TEST_PARSE_CFG_H_INCLUDED
+#define TEST_PARSE_CFG_H_INCLUDED
+
 void make_cfg_suite(void);
+
+
+
+#endif
--- test_statistics.h
+++ test_statistics.h
@@ -1,1 +1,7 @@
+#ifndef TEST_STATISTICS_H_INCLUDED
+#define TEST_STATISTICS_H_INCLUDED
+
 void make_statistics_suite(void);
+
+
+#endif
